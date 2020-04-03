d655a061acb628045af7d948e62a61ed0a23c891
avtakhov <avtahovfarit@gmail.com>
2020-04-03 22:27:56 +0300

    chenged junit structure

--- src/parser/test/ChainParserTest.java
+++ src/parser/test/ChainParserTest.java
@@ -1,68 +1,83 @@
 package parser.test;
 
+import base.Asserts;
 import org.junit.Test;
 import parser.ChainParser;
 import expression.TypeException;
 import parser.ParserException;
 
-import java.util.List;
-
-public class ChainParserTest extends BaseTest {
+public class ChainParserTest {
 
     private final ChainParser parser = new ChainParser();
 
     @Test
-    public void runCorrect() throws ParserException {
-        List<String> tests = List.of(
-                "filter{(1<2)}%>%map{element}",
-                "filter{(element>0)}%>%map{(element*2)}",
-                "filter{(element>10)}%>%filter{(element<20)}",
-                "map{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}",
-                "map{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}",
-                "filter{(element>0)}%>%filter{(element<0)}%>%map{(element*element)}",
-                "map{((element*element)*(element-1))}%>%filter{(((element*2)-(element-1))=1)}",
-                "map{(element+(element-element))}%>%map{((element-element)+1)}",
-                "map{12}%>%map{(element*2)}%>%filter{(1=0)}",
-                "filter{(((element*2)*(element+1))<0)}%>%map{(element+1000)}%>%map{(element+1000)}%>%map{(element+1000)}%>%filter{(element>0)}"
-        );
-        run(tests);
+    public void map() throws ParserException {
+        String tmp = "map{(element+1)}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
     }
 
     @Test
-    public void runSyntaxError() throws ParserException {
-        List<String> tests = List.of(
-                "filter{(1<2)}%>%map{element",
-                "filter{(element>0)}%%map{(element*2)}",
-                "filter{(elemnt>10)}%>%filter{(element<20)}",
-                "map{(element10)}%>%filter{(element>10)}%>%map{(element*element)}",
-                "emap{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}",
-                "filt$er{(element>0)}%>%filter{(element<0)}%>%map{(element*element)}",
-                "map{((element*element)*(element-1))}%>%filter{(((element*2)--(element-1))=1)}",
-                "map{(element+(element-element)}%>%map{((element-element)+1)}",
-                "map{12}%>%map{(element*2)}%>%filter{(1>=0)}",
-                "filter{(((element*2)*(element+1))<0)}%>%map{(element+1000)}%>%map{(element+1000)}%>%map{(element+1000)}%>%filter{(-element>-100)}",
-                "map{12}%>%map{(element*2)}%<%filter{(1=0)}",
-                "map{12}%>%mapp{(element*2)}%>%filter{(1=0)}",
-                "map{12}%>%mapp{(element*2)}%>%filter{(1>0)}{(1=0)}",
-                "adiaspdoaisdopasd",
-                "1",
-                "(1+2)"
-        );
-        runExpectingError(tests, ParserException.class);
+    public void filter() throws ParserException {
+        String tmp = "filter{(-1>(element*element))}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
     }
 
     @Test
-    public void runTypeError() throws ParserException {
-        List<String> tests = List.of(
-                "filter{(1+element)}",
-                "map{((1+element)<1)}"
-
-        );
-        runExpectingError(tests, TypeException.class);
+    public void simpleCompose() throws ParserException {
+        String tmp = "filter{(1<2)}%>%map{element}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
     }
 
-    @Override
-    void testOne(String test) throws ParserException {
-        parser.simplify(test);
+    @Test
+    public void mapFilterMap() throws ParserException {
+        String tmp = "map{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
     }
+
+    @Test
+    public void mapMap() throws ParserException {
+        String tmp = "map{(element+(element-element))}%>%map{((element-element)+1)}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
+    }
+
+    @Test(expected = ParserException.class)
+    public void endBracket() throws ParserException {
+        String tmp = "filter{(1<2)}%>%map{element";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
+    }
+    @Test(expected = ParserException.class)
+    public void separatorMiddleMissing() throws ParserException {
+        String tmp = "filter{(element>0)}%%map{(element*2)}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
+    }
+
+    @Test(expected = ParserException.class)
+    public void mapName() throws ParserException {
+        String tmp = "emap{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
+    }
+
+    @Test(expected = ParserException.class)
+    public void separatorMiddleAnother() throws ParserException {
+        String tmp = "map{12}%>%map{(element*2)}%<%filter{(1=0)}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
+    }
+    @Test
+    public void hard() throws ParserException {
+        String tmp = "filter{(((element*2)*(element+1))<0)}%>%map{(element+1000)}%>%map{(element+1000)}%>%map{(element+1000)}%>%filter{(element>0)}";
+        System.out.println(tmp + "    " + parser.simplify(tmp));
+    }
+
+    @Test(expected = TypeException.class)
+    public void mapBool() throws ParserException {
+        String tmp = "map{(element>0)}";
+        parser.simplify(tmp);
+    }
+
+    @Test(expected = TypeException.class)
+    public void filterInt() throws ParserException {
+        String tmp = "filter{42}";
+        parser.simplify(tmp);
+    }
+
 }
--- src/parser/test/ExpressionParserTest.java
+++ src/parser/test/ExpressionParserTest.java
@@ -2,80 +2,232 @@ package parser.test;
 
 import base.Asserts;
 import expression.TypeException;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import parser.*;
 import parser.ParserException;
 
-import java.util.List;
-
-public class ExpressionParserTest extends BaseTest {
+public class ExpressionParserTest {
 
     ExpressionParser parser = new ExpressionParser();
 
-    @Override
-    protected void testOne(String test) throws ParserException {
-        System.out.println("testing: " + test);
-        Asserts.assertEquals("parser error", test, parser.parse(test).toString());
+    @Test
+    public void cnst() throws ParserException {
+        String tmp = "42";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
     }
 
     @Test
-    public void runCorrectTests() throws Exception {
-        List<String> tests = List.of(
-                "10",
-                "element",
-                "(-1+2)",
-                "(element+1)",
-                "(-1*element)",
-                "(element*(element+3))",
-                "((1+element)-(element*element))",
-                "(1=1)",
-                "((element<0)|(element>0))",
-                "(((element+123)*(3*-2))=0)",
-                "((1=-2)|(element>((1--3)+(element*3))))",
-                "(-1--1)"
-        );
-        run(tests);
+    public void negate() throws ParserException {
+        String tmp = "-10";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
     }
 
     @Test
-    public void runSyntaxErrorTests() throws ParserException {
-        List<String> tests = List.of(
-                "100000000000000000000000000000",
-                "102-1",
-                "(102)",
-                "123@",
-                "(1+elements)",
-                "(element^element)",
-                "(element+1",
-                "(elemen/element)",
-                "(77/111 )",
-                "(1 +3)",
-                "(1++3)",
-                "element-1)",
-                "(1=1&1=1)",
-                "((1<2)||(1>2))",
-                "(element+element))",
-                "(>1)",
-                "(+element)",
-                "(--1--1)"
-        );
-        runExpectingError(tests, ParserException.class);
+    public void minInt() throws ParserException {
+        String tmp = Integer.toString(Integer.MIN_VALUE);
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
     }
 
     @Test
-    public void runTypeErrorTests() throws ParserException {
-        List<String> tests = List.of(
-                "(1+(1>element))",
-                "(1=(3=3))",
-                "(element<(element<0))",
-                "((1>2)+(3=3))",
-                "((1+2)&3)",
-                "(1+((element+1)=3))",
-                "((1000*1000)=(element=element))",
-                "(((element*element)+(element-1))<((1+2)<(1+3))))"
-        );
-        runExpectingError(tests, TypeException.class);
+    public void element() throws ParserException {
+        String tmp = "element";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void simple() throws ParserException {
+        String tmp = "(1+2)";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void elementGreater() throws ParserException {
+        String tmp = "(element>0)";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void elementLess() throws ParserException {
+        String tmp = "(element<10)";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void medium() throws ParserException {
+        String tmp = "((element<0)|(element=0))";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void mediumConst() throws ParserException {
+        String tmp = "(20391093+(-1+(-1--913993929)))";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void bool() throws ParserException {
+        String tmp = "(((-1<2)|(1>(2-1)))|(element>1000))";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void mediumMinInt() throws ParserException {
+        String minInt = Integer.toString(Integer.MIN_VALUE);
+        String tmp = "((" + minInt + "-" + minInt + ")>(element*" + minInt + "))";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test
+    public void hard() throws ParserException {
+        String tmp = "((((1+2)*3)-10300300)-((element*element)+(element+element)))";
+        Asserts.assertEquals("", tmp, parser.parse(tmp).toString());
+    }
+
+    @Test(expected = ParserException.class)
+    public void noEndBracket() throws ParserException {
+        String tmp = "(1+2";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void noStartBracket() throws ParserException {
+        String tmp = "1+2)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void overflowConstant() throws ParserException {
+        String tmp = "1000000000000000000000000";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void invalidElement() throws ParserException {
+        String tmp = "elementt";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void invalidElement2() throws ParserException {
+        String tmp = "elemen";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void invalidElement3() throws ParserException {
+        String tmp = "elemeent";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void insertSymbolMiddle() throws ParserException {
+        String tmp = "(1#0-3)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void insertSymbolEnd() throws ParserException {
+        String tmp = "(10-3)#";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void fullInvalid() throws ParserException {
+        String tmp = "-8t4t2t";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void noFirstArgument() throws ParserException {
+        String tmp = "(>element)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void noLastArgument() throws ParserException {
+        String tmp = "(element=)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void onlyBrackets() throws ParserException {
+        String tmp = "()";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void elementInBrackets() throws ParserException {
+        String tmp = "(element)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void invalidOperator() throws ParserException {
+        String tmp = "(element*|*element)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void doubleMinus() throws ParserException {
+        String tmp = "(element--element)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void noBrackets() throws ParserException {
+        String tmp = "(element>0)-(123+123)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = ParserException.class)
+    public void spaces() throws ParserException {
+        String tmp = "(1 + 2)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = TypeException.class)
+    public void equalToBool() throws ParserException {
+        String tmp = "((1+2)=(element>1))";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = TypeException.class)
+    public void lessInt() throws ParserException {
+        String tmp = "(element<(element<element))";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = TypeException.class)
+    public void AndInt() throws ParserException {
+        String tmp = "((element>1)&0)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = TypeException.class)
+    public void OrIntInt() throws ParserException {
+        String tmp = "(1|1)";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = TypeException.class)
+    public void MulBool() throws ParserException {
+        String tmp = "(((element+element)<42)*(77+42))";
+        parser.parse(tmp);
+    }
+    @Test(expected = TypeException.class)
+    public void SubBoolBool() throws ParserException {
+        String tmp = "(((1+2)<3)-((element=element)&(element>element)))";
+        parser.parse(tmp);
+    }
+
+    @Test(expected = TypeException.class)
+    public void EqBoolBool() throws ParserException {
+        String tmp = "((1=1)=(element=(42+42)))";
+        parser.parse(tmp);
+    }
+    @Test(expected = TypeException.class)
+    public void hardType() throws ParserException {
+        String tmp = "(((1+2)*(3+4))*(((5+6)*(7+8))*(element>(element*element))))";
+        parser.parse(tmp);
     }
 }
